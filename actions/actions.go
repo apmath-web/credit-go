5e90867b8fdee4e3fa8274de55d1e1cb1941da3a
korwin <levozavr@mail.ru>
2018-11-07 16:58:51 +0300

    fix a little bugs in example

--- actions/actions.go
+++ actions/actions.go
@@ -6,7 +6,7 @@ import (
 
 func Handle(response http.ResponseWriter, request *http.Request) {
 	url := request.URL
-	if url.Path == "/cregit" && request.Method == "POST" {
+	if url.Path == "/credit" && request.Method == "POST" {
 		Create(response, request)
 	}
 	// fetch and display errors here
