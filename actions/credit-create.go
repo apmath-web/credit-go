af459a9bc84b8b0bfd257302ae457c260e1d28f2
korwin <levozavr@mail.ru>
2018-11-18 23:04:10 +0300

    update actions

--- actions/credit-create.go
+++ actions/credit-create.go
@@ -1,9 +1,7 @@
 package actions
 
 import (
-	"encoding/json"
 	"fmt"
-	"github.com/apmath-web/credit-go/valueObjects"
 	"github.com/apmath-web/credit-go/viewModels"
 	"net/http"
 )
@@ -11,55 +9,19 @@ import (
 func Create(response http.ResponseWriter, request *http.Request) {
 	response.Header().Add("Content-Type",
 		"application/json; charset=utf-8")
-	jsonData, ok := ToJson(response, request)
-	if !ok {
+	jsonData := toJson(response, request)
+	if jsonData != nil {
 		return
 	}
 	creditViewModel := new(viewModels.Credit)
 	creditViewModel.Fill(jsonData)
-	ok = creditViewModel.Validate()
+	ok := creditViewModel.Validate()
 	if !ok {
-		jsonData := PtrMessagesToJsonErrMessage("Validation error",
+		jsonData := ptrMessagesToJsonErrMessage("Validation error",
 			creditViewModel.GetValidation().GetMessages())
 		response.WriteHeader(400)
 		fmt.Fprint(response, jsonData)
 		return
 	}
 	fmt.Fprintf(response, "{\"id\":1}")
-}
-
-func ToJson(response http.ResponseWriter, request *http.Request) (map[string]interface{}, bool) {
-	decoder := json.NewDecoder(request.Body)
-	var jsonData map[string]interface{}
-	err := decoder.Decode(&jsonData)
-	if err != nil {
-		jsonData := PtrMessagesToJsonErrMessage("Validation error",
-			[]valueObjects.MessageInterface{
-				valueObjects.GenMessage("package", err.Error())})
-		response.WriteHeader(400)
-		fmt.Fprint(response, jsonData)
-		return nil, false
-	}
-	return jsonData, true
-}
-
-func PtrMessagesToJsonErrMessage(message string,
-	descriptionPtr []valueObjects.MessageInterface) string {
-	if descriptionPtr == nil {
-		return "{\"message\":\"" + message + "\"}"
-	}
-	var description map[string]interface{}
-	description = make(map[string]interface{})
-	for _, item := range descriptionPtr {
-		description[item.GetField()] = item.GetText()
-	}
-	jsonData := map[string]interface{}{
-		"message":     message,
-		"description": description,
-	}
-	res, err := json.Marshal(jsonData)
-	if err != nil {
-		return "{\"message\":\"some error on server\"}"
-	}
-	return string(res)
 }
