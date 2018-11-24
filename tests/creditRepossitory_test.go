78ec44f84204765bafe38d11d2c93d9617b6c346
korwin <levozavr@mail.ru>
2018-11-24 12:57:27 +0300

    fix error

--- tests/creditRepossitory_test.go
+++ tests/creditRepossitory_test.go
@@ -1,16 +1,16 @@
 package tests
 
 import (
+	"github.com/apmath-web/credit-go/actions"
 	"github.com/apmath-web/credit-go/data"
 	"github.com/apmath-web/credit-go/models"
-	"github.com/apmath-web/credit-go/repositories"
 	"github.com/apmath-web/credit-go/valueObjects"
 	"reflect"
 	"testing"
 )
 
 func TestCreditRepositoryAddCredit(t *testing.T) {
-	repo := repositories.Repository
+	repo := actions.Repository
 	person := valueObjects.GenPerson("Alexandra", "Chernyshova")
 	credit := models.GenCredit(person, 1000, data.Str2Date("2018-10-10"), data.RUR, 10, 10)
 	repo.Store(credit)
