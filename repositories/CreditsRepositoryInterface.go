17079f62a6525fb08881f0c14e2b574045b76cab
varlogen <martynovpavel8@ya.ru>
2018-12-04 19:31:35 +0300

    удаление кредиита из репозитория

--- repositories/CreditsRepositoryInterface.go
+++ repositories/CreditsRepositoryInterface.go
@@ -5,5 +5,5 @@ import "github.com/apmath-web/credit-go/models"
 type CreditsRepositoryInterface interface {
 	Get(id int) models.CreditInterface
 	Store(credit models.CreditInterface)
-	Remove(credit models.CreditInterface) error
+	Remove(credit models.CreditInterface)
 }
