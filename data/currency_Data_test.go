e7efbfe223a2bb52b153c9c3fd38ba86b464d316
korwin <levozavr@mail.ru>
2018-12-15 05:04:16 +0300

    add tests for currency

---
+++ data/currency_Data_test.go
@@ -0,0 +1,74 @@
+package data
+
+import (
+	"github.com/franela/goblin"
+	"strconv"
+	"testing"
+)
+
+func TestMessageValueObject(t *testing.T) {
+	g := goblin.Goblin(t)
+	type TestData struct {
+		currency string
+	}
+	var testCurrency Currency
+	var testDataObject string
+	var positiveTestData, negativeTestData []TestData
+	var numberOfTestsPos, numberOfTestsNeg int
+	g.Describe("Currency data tests", func() {
+		g.Before(func() {
+			positiveTestData = []TestData{
+				{"USD"},
+				{"EUR"},
+				{"RUR"},
+			}
+			negativeTestData = []TestData{
+				{""},
+				{"RUB"},
+				{"rur"},
+				{"usd"},
+				{"eur"},
+				{" "},
+				{"something"},
+				{"E U R"},
+				{"TeSt"},
+			}
+			numberOfTestsNeg = len(negativeTestData)
+			numberOfTestsPos = len(positiveTestData)
+		})
+		g.It("Positive tests for currency", func() {
+			for i := 0; i < numberOfTestsPos; i++ {
+				g.Describe("Test #"+strconv.Itoa(i+1), func() {
+					g.Before(func() {
+						testCurrency = Currency(positiveTestData[i].currency)
+						testDataObject = positiveTestData[i].currency
+					})
+					g.It("str2cur test", func() {
+						g.Assert(Str2Cur(testDataObject)).Equal(testCurrency)
+					})
+					g.It("cur2str test", func() {
+						g.Assert(testCurrency.Cur2Str()).Equal(testDataObject)
+
+					})
+				})
+			}
+		})
+		g.It("Negative tests for currency", func() {
+			for i := 0; i < numberOfTestsNeg; i++ {
+				g.Describe("Test #"+strconv.Itoa(i+1), func() {
+					g.Before(func() {
+						testCurrency = Currency(negativeTestData[i].currency)
+						testDataObject = negativeTestData[i].currency
+					})
+					g.It("str2cur test", func() {
+						g.Assert(Str2Cur(testDataObject)).Equal(Currency(""))
+					})
+					g.It("cur2str test", func() {
+						g.Assert(testCurrency.Cur2Str()).Equal("")
+
+					})
+				})
+			}
+		})
+	})
+}
