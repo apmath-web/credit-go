8df066c4471f6fbf01cfcaae6c94dc118c59dfc2
korwin <levozavr@mail.ru>
2018-12-15 14:47:19 +0300

    replace all tests

--- models/credit_Models_test.go
+++ models/credit_Models_test.go
@@ -1,25 +1,24 @@
-package tests
+package models
 
 import (
 	"github.com/apmath-web/credit-go/data"
-	"github.com/apmath-web/credit-go/models"
 	"github.com/apmath-web/credit-go/valueObjects"
 	"testing"
 )
 
 func TestCreditModelCreation(t *testing.T) {
 	person := valueObjects.GenPerson("Alexandra", "Chernyshova")
-	_, err := models.GenCredit(person, 100, data.Str2Date("2018-10-10"), data.RUR, 10, 10)
+	_, err := GenCredit(person, 100, data.Str2Date("2018-10-10"), data.RUR, 10, 10)
 	if err == nil {
 		t.Errorf("Logic for small payment does not work.")
 		return
 	}
-	_, err = models.GenCredit(person, 1200, data.Str2Date("2018-10-10"), data.RUR, 1200, 10)
+	_, err = GenCredit(person, 1200, data.Str2Date("2018-10-10"), data.RUR, 1200, 10)
 	if err == nil {
 		t.Errorf("Logic for small rounding does not work.")
 		return
 	}
-	_, err = models.GenCredit(person, 1200, data.Str2Date("2018-10-10"), data.RUR, 10, 10)
+	_, err = GenCredit(person, 1200, data.Str2Date("2018-10-10"), data.RUR, 10, 10)
 	if err != nil {
 		t.Errorf("Starnge error.")
 		return
